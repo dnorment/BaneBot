5ed2a1ebd78136e7b707b52daa736db8a761e5cc
Daniel Norment <danielnorment@gmail.com>
2020-07-29 15:22:03 -0700

    Fix JDABuilder deprecation

--- src/main/java/norment/banebot/Bane.java
+++ src/main/java/norment/banebot/Bane.java
@@ -1,9 +1,9 @@
 package norment.banebot;
 
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
 
@@ -19,11 +19,17 @@ public class Bane {
         String token = cfg.properties.getProperty("botToken");
         prefix = cfg.properties.getProperty("prefix");
 
-        //Set bot online
-        JDABuilder builder = new JDABuilder(AccountType.BOT);
-        builder.setToken(token);
-        builder.setStatus(OnlineStatus.ONLINE);
-        builder.setActivity(Activity.watching("the fire rise"));
+        //Set bot token and allowed events
+        JDABuilder builder = JDABuilder.create(
+                token,
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                GatewayIntent.GUILD_VOICE_STATES
+        );
+
+        //Set visible bot status
+        builder.setStatus(OnlineStatus.ONLINE)
+                .setActivity(Activity.watching("the fire rise"));
 
         //Listen for events and handle
         builder.addEventListeners(new Commands());
