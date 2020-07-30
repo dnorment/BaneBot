3542538c03601eeb35578344137bbc5c2b36884a
Daniel Norment <danielnorment@gmail.com>
2020-07-29 21:44:39 -0700

    Add help command

--- src/main/java/norment/banebot/command/Command.java
+++ src/main/java/norment/banebot/command/Command.java
@@ -4,7 +4,13 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public abstract class Command {
     public abstract String getCommand();
+
     public abstract String getDescription();
+
     public abstract String[] getUsage();
+
     public abstract void run(GuildMessageReceivedEvent event);
+
+    public static void showUsage(GuildMessageReceivedEvent event, Command cmd, boolean error) {
+    }
 }
