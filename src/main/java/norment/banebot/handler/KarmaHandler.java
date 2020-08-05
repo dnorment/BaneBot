e07ca3c6e87b0751872672b62cac7228bc07ea6e
Daniel Norment <danielnorment@gmail.com>
2020-08-05 13:14:26 -0700

    Register ReactionEmote ids in DB as voting

--- src/main/java/norment/banebot/handler/KarmaHandler.java
+++ src/main/java/norment/banebot/handler/KarmaHandler.java
@@ -2,6 +2,7 @@ package norment.banebot.handler;
 
 import com.mongodb.client.MongoCollection;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Emote;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.MessageReaction.ReactionEmote;
 import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
@@ -15,6 +16,49 @@ public class KarmaHandler {
 
     public static void init(JDA jda) {
         MongoCollection<Document> karmaCollection = DatabaseHandler.karmaCollection;
+
+        for (Document doc : karmaCollection.find()) {
+            String guildId = doc.get("guild").toString();
+            String upvoteId = doc.get("upvote").toString();
+            String downvoteId = doc.get("downvote").toString();
+
+            Guild guild = jda.getGuildById(guildId);
+            if (guild == null) continue;
+
+            boolean upvoteRegistered = false;
+            boolean downvoteRegistered = false;
+
+            //check if upvote reaction is a custom emote from guild
+            for (int i = 0; i < guild.getEmotes().size(); i++) {
+                Emote emote = guild.getEmotes().get(i);
+                if (emote.getId().equals(upvoteId)) {
+                    ReactionEmote upvoteReaction = ReactionEmote.fromCustom(emote);
+                    upvoteReactions.put(guild, upvoteReaction);
+                    upvoteRegistered = true;
+                    break;
+                }
+            }
+            //is not a custom reaction, treat as unicode emoji
+            if (!upvoteRegistered) {
+                ReactionEmote upvoteReaction = ReactionEmote.fromUnicode(upvoteId, jda);
+                upvoteReactions.put(guild, upvoteReaction);
+            }
+
+            //repeat for downvote
+            for (int i = 0; i < guild.getEmotes().size(); i++) {
+                Emote emote = guild.getEmotes().get(i);
+                if (emote.getId().equals(downvoteId)) {
+                    ReactionEmote downvoteReaction = ReactionEmote.fromCustom(emote);
+                    downvoteReactions.put(guild, downvoteReaction);
+                    downvoteRegistered = true;
+                    break;
+                }
+            }
+            if (!downvoteRegistered) {
+                ReactionEmote downvoteReaction = ReactionEmote.fromUnicode(downvoteId, jda);
+                upvoteReactions.put(guild, downvoteReaction);
+            }
+        }
     }
 
     public static void handleReaction(GuildMessageReactionAddEvent event) {
