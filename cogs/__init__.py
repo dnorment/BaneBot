ca6a33d3b8ae890323341eb65074c571764c198d
Daniel Norment <danielnorment@gmail.com>
2021-10-04 14:39:24 -0700

    Import top-level cogs only

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -2,5 +2,13 @@ from os import listdir
 from os.path import basename, dirname
 
 # list all names of cogs
-cog_names = [basename(f)[:-3] for f in listdir(dirname(__file__))
-             if f[-3:] == ".py" and not f.endswith("__init__.py")]
+cog_names: list[str] = []
+for f in listdir(dirname(__file__), ):
+    is_file = '.' in basename(f)
+    is_top_level = is_file and basename(f).count('.') == 1
+    is_py_file = is_top_level and f.endswith('.py')
+    is_init_py = f.endswith('__init__.py')
+
+    if is_py_file and not is_init_py:
+        cog_name = basename(f)[:-3]
+        cog_names.append(cog_name)
