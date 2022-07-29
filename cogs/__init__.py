b939faa097db4d0506a3660d8a9aa080ddc781a5
Daniel Norment <danielnorment@gmail.com>
2022-07-28 17:48:24 -0700

    Clean up cog import, support subfolders

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,15 +1,20 @@
-from os import listdir
-from os.path import basename, dirname
-from typing import List
+import os
+from typing import Set
 
-# list all names of cogs
-cog_names: List[str] = []
-for f in listdir(dirname(__file__), ):
-    is_file = '.' in basename(f)
-    is_top_level = is_file and basename(f).count('.') == 1
-    is_py_file = is_top_level and f.endswith('.py')
-    is_init_py = f.endswith('__init__.py')
 
-    if is_py_file and not is_init_py:
-        cog_name = basename(f)[:-3]
-        cog_names.append(cog_name)
+def _get_cog_names():
+    '''Get a list of all cogs to be loaded on startup. Cogs must be defined
+    in `./cogs` or in a subfolder's `cog.py` file.'''
+
+    cog_names: Set[str] = set()
+
+    files = os.listdir('./cogs')
+    for f in files:
+        if f in ['__init__.py', '__pycache__']:
+            continue
+
+        cog_names.add(f[:-3] if f.endswith('.py') else f'{f}.cog')
+
+    return cog_names
+
+cog_names = _get_cog_names()
