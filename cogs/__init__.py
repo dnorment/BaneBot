0799bff68351cf7f8d197413abe01b07faaa53c1
Daniel Norment <daniel@norment.dev>
2023-07-15 23:53:52 -0700

    Style changes

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,20 +1,41 @@
+import logging
 import os
+from datetime import datetime, timedelta
 from typing import Set
 
+import disnake
+from disnake.ext import commands
 
-def _get_cog_names():
-    '''Get a list of all cogs to be loaded on startup. Cogs must be defined
-    in `./cogs` or in a subfolder's `cog.py` file.'''
 
-    cog_names: Set[str] = set()
+class BaneCog(commands.Cog):
+    logger: logging.Logger
 
-    files = os.listdir('./cogs')
-    for f in files:
-        if f in ['__init__.py', '__pycache__']:
-            continue
+    async def cog_load(self):
+        cog_name = type(self).__name__.lower()
+        self.logger = logging.getLogger(f'cogs.{cog_name}')
 
-        cog_names.add(f[:-3] if f.endswith('.py') else f'{f}.cog')
+        self.logger.info('Loaded cog')
 
-    return cog_names
+    def cog_unload(self):
+        self.logger.info('Unloaded cog')
 
-cog_names = _get_cog_names()
+    @staticmethod
+    def message_older_than_24h(message: disnake.Message) -> bool:
+        '''
+        True if `message` is older than 24 hours.
+        '''
+
+        now_timestamp = datetime.now().timestamp()
+        seconds_in_24h = timedelta(days=1).total_seconds()
+        timestamp_24h_ago = now_timestamp - seconds_in_24h
+
+        if timestamp_24h_ago >= message.created_at.timestamp():
+            return True
+
+        return False
+
+
+# Cogs must be defined in `/cogs` or a subfolder's `cog.py` file
+_cog_files = set(os.listdir('./cogs')) - {'__init__.py', '__pycache__'}
+for f in _cog_files:
+    cog_names = {f[:-3] if f.endswith('.py') else f'{f}.cog' for f in _cog_files}
